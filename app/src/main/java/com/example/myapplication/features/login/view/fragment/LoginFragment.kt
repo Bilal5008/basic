4e77c987a74dc81b5923b5bb7a6705c34d43d2c9
HAMMAD-KHAN\HP <bilal@bestercapitalmedia.com>
2021-05-21 16:23:57 +0500

    'basicstructure'

---
+++ app/src/main/java/com/example/myapplication/features/login/view/fragment/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.features.login.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    var fragmentLoginBinding: FragmentLoginBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        return fragmentLoginBinding?.root
+    }
+
+
+}
